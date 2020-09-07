60c3a994b5068a5cd17b0fc95ee2accd9707f8c2
RedRubiksCube <45070667+RedRubiksCube@users.noreply.github.com>
2020-09-07 10:07:20 -0400

    graphing complete

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "750ca915d2b4b0af7e1b5991c1b3c15f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBZkDY_t6Qga4Gm3k7qYZO5PDJWk5sslgo"
